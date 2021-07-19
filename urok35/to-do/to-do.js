9a834610b84b1c7023b78eb2c5598230a30b0209
Mukhamed Masanov <muhamedmasanov075@gmail.com>
2021-07-19 12:08:03 +0700

    finished todo list

--- urok35/to-do/to-do.js
+++ urok35/to-do/to-do.js
@@ -4,7 +4,7 @@ $(() => {
     let $newItemForm = $('#newItemForm')
     let $newItemButton = $('#newItemButton')
     let $itemDescription = $('#itemDescription')
-    let $li = $('li');
+    let $li = $(' #list  > li');
     let itemText = ''; //будет хранить текст из текстового поля
     //Задача 1: Скрывваем начальный список и заетм плавно его выводим по элементна и с задержкой
     $li.hide().each(function (index) {
@@ -35,28 +35,24 @@ $(() => {
         event.preventDefault()
         const text = $itemDescription.val().trim();
         let $time = new Date();
-$('.time').after(`<p>${$time.getFullYear()}:${$time.getMonth()}:${$time.getDate()}</p>`)
+        let $date = `${$time.getDate() > 10 ? $time.getDate() : `0${$time.getDate()}`}:${$time.getMonth() > 10 ? $time.getMonth() + 1 : `0${$time.getMonth() + 1}`}:${$time.getFullYear()}`
+        let $exactTime = `${$time.getHours() > 10 ? $time.getHours() : `0${$time.getHours()}`}:${$time.getMinutes() > 10 ? $time.getMinutes() : `0${$time.getMinutes()}`}`
+        let $appendLi = '';
         if (text.length !== 0) {
-            $list.prepend(`<li class="time">${text}</li>`)
+            $appendLi = $(`<li><i class="fas fa-trash"></i> <span> ${text} </span> <span>${$date}</span> <span>${$exactTime} </span> <i class="fas fa-pen-square"></i> </li>`).on('click', 'i.fa-trash', function () {
+                $(this).closest('li').remove()
+                $.cookie('liItem', $appendLi)
+                updateCounter()
+            })
+            $list.prepend($appendLi)
             $itemDescription.val('');
             updateCounter();
         }
     })
 
-    // $newItemForm.on('submit', function (e) {
-    //     e.preventDefault()
-    //
-    //     const text = $itemDescription.val().trim(); //берем значения атрибута value //trim - убирает пробелы ,
-    //     // табуляцию , переносы на новую строку в начале и в конце строки
-    //     if (text.length !== 0) {
-    //         $list.prepend(`<li>${text}</li>`)
-    //         $itemDescription.val('');
-    //         updateCounter();
-    //     }
-    // })
-
     //удаление элементов , при первом нажатии на элемент мы делаем егокрасным цветом и помещаем  в конец списка . При повторном нажатии мы удаляем элемент
-    $list.on('click', 'li', function () {
+    $list.on('click', 'li  ', function () {
+
         let $elem = $(this); //кэшируем элемент . т.е сохранит его нажатым его в нажатом состоянии
         let $complete = $elem.hasClass('complete') //проверяем есть ли у элемента класс complete. Return boolean
         if ($complete) {
@@ -68,7 +64,29 @@ $('.time').after(`<p>${$time.getFullYear()}:${$time.getMonth()}:${$time.getDate(
             itemText = $elem.text() //беру текст из нажатого элемента списка
             $elem.remove()
             //Добавление в конце списка с классом complete
-            $list.append(`<li class="complete">${itemText}</li>`).hide().fadeIn(500)
+            $list.append(`<li class="complete"><span>${itemText}</span></li>`).hide().fadeIn(500)
         }
+
+    })
+
+    $('.fa-trash').on('click', function () {
+        $(this).closest('li').remove()
+        updateCounter()
+
+    })
+    let currentVal = '';
+    $list.on('click', '.fa-pen-square', function (event) {
+        event.stopPropagation()
+        currentVal = $(this).closest('li').find('span').text()
+        $(this).closest('li').find('span').eq(0).prop('contentEditable', 'true').focus();
+    })
+    $("li > span").on('keydown', function (e) {
+        if (e.which === 13) {
+            e.preventDefault()
+            let $newItem = $(this).text().trim()
+            $newItem.length === 0 ? $(this).text(`${currentVal}`) : ''
+            $(this).blur()
+        }
+
     })
 })
